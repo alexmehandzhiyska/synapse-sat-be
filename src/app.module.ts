e0c71ecdabb0ebbd98cbc12bff72b17a1d74eca5
Alexandrina Mehandzhiyska <73063309+alexmehandzhiyska@users.noreply.github.com>
2026-07-04 10:10:23 +0300

    config: connect to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'synapse_sat',
+      password: 'synapse_sat_password',
+      database: 'synapse_sat',
+      autoLoadEntities: true,
+      synchronize: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DataSource } from 'typeorm';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+
+    const dataSource = app.get(DataSource);
+
+    if (dataSource.isInitialized) {
+        console.log('Connected');
+    } else {
+        console.log('Not connected');
+    }
+
+    await app.listen(process.env.PORT ?? 5500);
 }
+
 bootstrap();
