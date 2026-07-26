407b7109042b9f44ea1765e057715664d2195ae9
Alexandrina Mehandzhiyska <73063309+alexmehandzhiyska@users.noreply.github.com>
2026-07-26 15:31:40 +0300

    feat(practice tests): add test listing

--- src/modules/practice-test/practice-test.module.ts
+++ src/modules/practice-test/practice-test.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PracticeTestService } from './practice-test.service';
 import { PracticeTestController } from './practice-test.controller';
+import { AnswerChoice } from './entities/answer-choice.entity';
+import { Module as PracticeTestModuleEntity } from './entities/module.entity';
+import { PracticeTest } from './entities/practice-test.entity';
+import { Question } from './entities/question.entity';
 
 @Module({
+    imports: [
+        TypeOrmModule.forFeature([
+            PracticeTest,
+            PracticeTestModuleEntity,
+            Question,
+            AnswerChoice,
+        ]),
+    ],
     controllers: [PracticeTestController],
     providers: [PracticeTestService],
 })
--- src/modules/practice-test/practice-test.service.ts
+++ src/modules/practice-test/practice-test.service.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreatePracticeTestDto } from './dto/create-practice-test.dto';
 import { UpdatePracticeTestDto } from './dto/update-practice-test.dto';
+import { PracticeTest } from './entities/practice-test.entity';
 
 @Injectable()
 export class PracticeTestService {
+    constructor(
+        @InjectRepository(PracticeTest)
+        private readonly practiceTestRepository: Repository<PracticeTest>,
+    ) { }
+
     create(createPracticeTestDto: CreatePracticeTestDto) {
         return 'This action adds a new practiceTest';
     }
 
-    findAll() {
-        return `This action returns all practiceTest`;
+    findAll(): Promise<PracticeTest[]> {
+        return this.practiceTestRepository.find({
+            relations: { modules: true },
+            order: {
+                createdAt: 'ASC',
+                modules: { section: 'ASC', position: 'ASC' },
+            },
+        });
     }
 
     findOne(id: number) {
