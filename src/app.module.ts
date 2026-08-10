82754f4737d4c2132e7e94a4721fa9124e3c89c0
Alexandrina Mehandzhiyska <73063309+alexmehandzhiyska@users.noreply.github.com>
2026-08-10 18:34:35 +0300

    refactor: switch to using local db

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,11 +14,11 @@ import { TestAttemptModule } from './modules/test-attempt/test-attempt.module';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'synapse_sat',
-      password: 'synapse_sat_password',
-      database: 'synapse_sat',
+      host: process.env.DB_HOST ?? 'localhost',
+      port: Number(process.env.DB_PORT ?? 5432),
+      username: process.env.DB_USER ?? 'postgres',
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME ?? 'synapse_sat',
       autoLoadEntities: true,
       synchronize: false
     }),
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,12 +1,13 @@
+import 'dotenv/config';
 import { DataSource } from 'typeorm';
 
 export default new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'synapse_sat',
-  password: 'synapse_sat_password',
-  database: 'synapse_sat',
+  host: process.env.DB_HOST ?? 'localhost',
+  port: Number(process.env.DB_PORT ?? 5432),
+  username: process.env.DB_USER ?? 'postgres',
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME ?? 'synapse_sat',
   entities: ['src/**/*.entity.ts'],
   migrations: ['src/migrations/*.ts'],
   synchronize: false,
