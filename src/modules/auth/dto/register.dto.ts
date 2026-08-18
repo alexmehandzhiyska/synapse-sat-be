7c88c2d252f47dca64611c0d2cf4edb0774cb2be
Alexandrina Mehandzhiyska <73063309+alexmehandzhiyska@users.noreply.github.com>
2026-08-18 13:56:57 +0300

    chore(validation): add user data validations

--- src/modules/auth/dto/register.dto.ts
+++ src/modules/auth/dto/register.dto.ts
@@ -1,19 +1,27 @@
-import { IsEmail, IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsOptional, IsString, Matches, MaxLength, MinLength } from 'class-validator';
 
 export class RegisterDto {
     @IsString()
     @IsNotEmpty()
+    @MaxLength(100)
     firstName: string;
 
     @IsString()
     @IsNotEmpty()
+    @MaxLength(100)
     lastName: string;
 
     @IsEmail()
+    @MinLength(5)
+    @MaxLength(200)
     email: string;
 
     @IsString()
     @MinLength(8)
+    @MaxLength(72)
+    @Matches(/(?=.*\d)(?=.*[^A-Za-z0-9])/, {
+        message: 'Password must contain at least 1 digit and 1 special character.',
+    })
     password: string;
 
     @IsOptional()
