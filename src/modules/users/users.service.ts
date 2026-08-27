9663aa585e01d5323c147a93bc74deb5ca3969c3
Alexandrina Mehandzhiyska <73063309+alexmehandzhiyska@users.noreply.github.com>
2026-08-27 16:20:18 +0300

    chore: sort user accounts by date created desc

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -11,7 +11,7 @@ export class UsersService {
     ) { }
 
     async findAll() {
-        const users = await this.usersRepository.find({ order: { createdAt: 'ASC' } });
+        const users = await this.usersRepository.find({ order: { createdAt: 'DESC' } });
 
         return users.map((user) => this.toUserResponse(user));
     }
